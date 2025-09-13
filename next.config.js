12d93a1702704704d4d0a74b9dbbe8f3c0e5bdf0
Berima1 <Eddy199099@gmail.com>
2025-09-13 23:11:59 +0000

    Update next.config.js
    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-```javascript
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: []
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '1mb'
-    },
-    responseLimit: '8mb'
   }
 }
 
 module.exports = nextConfig
-```
